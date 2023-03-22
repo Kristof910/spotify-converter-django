c1fc6578c8edd10720cff8915f430323a09d1584
Bába Kristóf <kristof@Babas-MacBook-Pro.local>
2023-03-22 09:47:44 +0200

    base template + landing page

---
+++ yt_spoti_converter_django_application/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class YtSpotiConverterDjangoApplicationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'yt_spoti_converter_django_application'
